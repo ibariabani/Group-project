732631bbca3c724ec42eb211bd46cf0cd3067084
ibariabani <2128870a@student.gla.ac.uk>
2017-02-14 16:38:17 +0000

    Delete Analytical1.h

--- Analytical1.h
+++
@@ -1,8 +0,0 @@
-#include<iostream>  /*Includes all neccessary libraries*/
-#include<cmath>
-#include<cstdlib>
-#include"math.h"
-#include"fstream"
-
-
-void Analytical1(double& a,double& b, double& V0,double& delta);
