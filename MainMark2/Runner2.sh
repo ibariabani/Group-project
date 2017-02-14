63d4956558b2dd6387f5008058fd1f47c5066c14
g-rostagni <g.rostagni@outlook.com>
2017-02-14 16:14:10 +0000

    Now outputs all plots to a single file

--- MainMark2/Runner2.sh
+++ MainMark2/Runner2.sh
@@ -4,12 +4,36 @@ make                    # Makes the main program using the makefile
 clear
 ./Main                  #Runs the main function
 
+rm -rf Results.ps
+rm -rf Results.pdf
+touch Results.ps
 
 if [ -e "GS.dat" ]         #After the Main function has run, if the Gauss data file exists then plot it
 then                       #The next few lines are all GNUPLOT commands to give a graph
-./plotter.sh Jacobi GS SOR Analytical1 Analytical2
+./plotter.sh GS Analytical2
 fi
 
+if [ -e "Jacobi.dat" ]
+then
+./plotter.sh Jacobi
+fi
+
+if [ -e "SOR.dat" ]
+then
+./plotter.sh SOR
+fi
+
+if [ -e "Analytical1.dat" ]
+then
+./plotter.sh Analytical1
+fi
+
+if [ -e "Analytical2.dat" ]
+then
+./plotter.sh Analytical2
+fi
+
+rm -rf Results.ps 
 
 if [[ -e "Jacobidiff.dat" && -e "Gaussdiff.dat" && -e "SORdiff.dat" ]]
 then
