ad5deda6f91f74e0d4e57a4784f792f755800b2f
g-rostagni <g.rostagni@outlook.com>
2017-02-14 16:41:31 +0000

    new name format for output files

--- MainMark2/Runner2.sh
+++ MainMark2/Runner2.sh
@@ -8,48 +8,28 @@ rm -rf Results.ps
 rm -rf Results.pdf
 touch Results.ps
 
-if [ -e "GS.dat" ]         #After the Main function has run, if the Gauss data file exists then plot it
-then                       #The next few lines are all GNUPLOT commands to give a graph
-./plotter.sh GS Analytical2
-fi
-
-if [ -e "Jacobi.dat" ]
-then
-./plotter.sh Jacobi
-fi
-
-if [ -e "SOR.dat" ]
-then
-./plotter.sh SOR
-fi
-
-if [ -e "Analytical1.dat" ]
-then
-./plotter.sh Analytical1
-fi
-
-if [ -e "Analytical2.dat" ]
-then
-./plotter.sh Analytical2
-fi
+for file in ./v* #Plots all the v*.dat files containing the potential information
+do
+./plotter.sh $file
+done
 
 rm -rf Results.ps 
 
-if [[ -e "Jacobidiff.dat" && -e "Gaussdiff.dat" && -e "SORdiff.dat" ]]
+if [[ -e "dJacobi.dat" && -e "dGauss.dat" && -e "dSOR.dat" ]]
 then
 gnuplot <<EOF
 set terminal postscript color
 set logscale xy
 set output "diffplot.ps"
-plot "Jacobidiff.dat" with lines, "Gaussdiff.dat" with lines, "SORdiff.dat" with lines
+plot "dJacobi" with lines, "dGS" with lines, "dSOR" with lines
 
 EOF
 
 ps2pdf14 diffplot.ps
 rm -rf diffplot.ps
-rm Jacobidiff.dat
-rm Gaussdiff.dat
-rm SORdiff.dat
+rm dJacobi
+rm dGS
+rm dSOR
 fi
 
 exit 0
