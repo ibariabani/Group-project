888725a60109805c59c4926db38ff7ba12f836f5
CatrionaMarr <fanatapan@yahoo.co.uk>
2017-03-07 14:00:39 +0000

    Delete BConds.h

--- Main1/BConds.h
+++
@@ -1,9 +0,0 @@
-#include<iostream>  /*Includes all neccessary libraries*/
-#include<cmath>
-#include<cstdlib>
-#include"math.h"
-#include"fstream"
-#include"Main.h"
-
-
-int BConds();     /*Function Declaration*/
